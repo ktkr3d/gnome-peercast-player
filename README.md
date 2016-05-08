92f9bdff02675b9497e9824ea786e784c427ed02
ktkr3d <ktkr3d@gmail.com>
2016-05-08 20:24:47 +0900

    streaming and web browsing

--- README.md
+++ README.md
@@ -3,4 +3,9 @@ GNOME Peercast Player
 
 # Screenshots
 - 2016-05-08  
-  ![GNOME Peercast Player](gnome-peercast-player-20160508.png)
+  ![GNOME Peercast Player](http://ktkr3d.github.io/images/gnome-peercast-player-20160508-01.png)
+
+# Install
+```bash
+$ sudo apt-get install vlc apt-get install gir1.2-webkit-3.0
+```
--- gpp_main.py
+++ gpp_main.py
@@ -2,10 +2,15 @@ import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
+# vlc
 import time
 import sys
 import vlc
-import vlcwidget
+
+# webkit
+import gi
+gi.require_version('WebKit', '3.0')
+from gi.repository import WebKit
 
 from gettext import gettext as _
 
@@ -24,13 +29,20 @@ class VLCWidget(Gtk.DrawingArea):
             return True
         self.connect("map", handle_embed)
         self.set_size_request(640, 360)
+#        self.set_size_request(640, 660)
 
 
 class Handler:
     def on_window_delete_event(self, *args):
         Gtk.main_quit(*args)
-    def on_menu_clicked(self, popover):
-        popover.show_all()
+    def on_button_quit_clicked(self, window):
+        Gtk.main_quit(window)
+    def on_button_about_clicked(self, dialog_about):
+        dialog_about.run()
+        dialog_about.destroy()
+    def on_button_preferences_clicked(self, dialog_preferences):
+        dialog_preferences.run()
+        dialog_preferences.destroy()
 
 builder = Gtk.Builder()
 builder.add_from_file("gpp_main.glade")
@@ -40,15 +52,27 @@ window = builder.get_object("window")
 headerbar = builder.get_object("headerbar")
 window.set_titlebar(headerbar)
 
-# vlc staff
+# vlc
 stream = builder.get_object("stream")
 stream.__vlc = VLCWidget()
 stream.add(stream.__vlc)
 #stream.pack_start(vlc, True, True, 0)
 stream.player=stream.__vlc.player
+stream_url = "./galaxias.mp4"
+#stream_url = "mmsh://localhost:7144/stream/ ..."
+#stream_url = "http://localhost:7144/stream/ ..."
+stream.player.set_media(instance.media_new(stream_url))
 
-stream.player.set_media(instance.media_new("./galaxias.mp4"))
-#time.sleep(3)
+# list
+store_list = builder.get_object("store_list")
+store_list.append(["channel",100])
+
+# webkit
+web = builder.get_object("web")
+web_view = WebKit.WebView()
+web_url = "http://localhost:7144/"
+web_view.open(web_url)
+web.add(web_view)
 
 window.show_all()
 time.sleep(1)
